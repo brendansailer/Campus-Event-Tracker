cc5c1cba777a9accf885d46bf2ea2ddd3bfab3ce
brendansailer <57545584+brendansailer@users.noreply.github.com>
2021-05-10 15:47:55 -0400

    Improve look of events feed

--- FrontEnd/src/Components/IndividualEventPage/StandaloneEvent/StandaloneEvent.js
+++ FrontEnd/src/Components/IndividualEventPage/StandaloneEvent/StandaloneEvent.js
@@ -12,7 +12,7 @@ export default function StandaloneEvent(props) {
         <h5 className="event-text">START: {props.event_start}</h5>
         <h5 className="event-text">END: {props.event_end}</h5>
         <h5 className="event-text">DESCRIPTION: {props.description}</h5>
-        <h5 className="event-text">WHERE: {props.location}</h5>
+        {props.location && <h5 className="event-text">WHERE: {props.location}</h5>}
       </div>
     </div>
   );
