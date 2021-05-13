c05843fbfe834c230c6181e78d89806a2263455b
brendansailer <57545584+brendansailer@users.noreply.github.com>
2021-05-13 18:05:16 -0400

    Calendar has clickable links now

--- FrontEnd/src/Components/CalendarPage/Calendar/EventsCalendar.js
+++ FrontEnd/src/Components/CalendarPage/Calendar/EventsCalendar.js
@@ -1,10 +1,17 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Redirect } from "react-router-dom";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "./EventsCalendar.css"
 
 moment.locale();
 const localizer = momentLocalizer(moment);
+
+function redirect_to_event(event){
+    console.log(event.id)
+    window.location = "/event/" + event.id;
+    return (<Redirect to={"/event/" + event.id} />)
+}
 
 export default function EventsCalendar(props) {
     const transformedEvents = props.events.map(e => {
@@ -17,6 +24,7 @@ export default function EventsCalendar(props) {
                 localizer={localizer}
                 events={transformedEvents}
                 style={{ height: 600 }}
+                onSelectEvent={event => redirect_to_event(event)}
             />
         </div>
     )
