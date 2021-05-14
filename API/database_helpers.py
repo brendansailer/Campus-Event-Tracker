9f2cf18aff831c138131bb53f17a7467d858a3dc
Jack Fox <jfox13@nd.edu>
2021-05-14 14:01:09 -0400

    added a SessionPool to this branch

--- API/database_helpers.py
+++ API/database_helpers.py
@@ -4,10 +4,12 @@ import cx_Oracle
 db_user = os.environ.get('DBAAS_USER_NAME', 'project')
 db_password = os.environ.get('DBAAS_USER_PASSWORD', 'password')
 db_connect = os.environ.get('DBAAS_DEFAULT_CONNECT_DESCRIPTOR', "localhost:1521/XE")
-connection = cx_Oracle.connect(db_user, db_password, db_connect)
-
-def get_cursor():
-    return connection.cursor()
+pool = cx_Oracle.SessionPool(db_user, db_password, db_connect, min=1, max=20, increment=1, encoding="UTF-8")
 
 def get_connection():
-    return connection
+    con = pool.acquire()
+    return con, con.cursor()
+
+def close(connection, cursor):
+    cursor.close()
+    pool.release(connection)
