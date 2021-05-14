01a96f207b066bf736e0b7674d0322fadb932107
Chad Cocco <ccocco04@gmail.com>
2021-05-14 18:49:52 +0000

    rank member/leader showing

--- FrontEnd/src/Components/ProfilePage/UserProfile/SubscriptionTile.js
+++ FrontEnd/src/Components/ProfilePage/UserProfile/SubscriptionTile.js
@@ -4,9 +4,15 @@ import "./SubscriptionTile.css"
 
 export default function SubscriptionTile(props) {
   // const date = new Date(props.event_start);
+  const rankText = (rank) => {
+    if (rank === "0")
+      return "member"
+    if (rank === "1")
+      return "leader"
+  }
   return (
     <div className="subscription-tile">
-      <span className="club-name"><p>{props.club_name}</p><p>{props.rank}</p></span>
+      <span className="club-name"><p>{props.club_name}</p><p className="rank-text"><em>{rankText(props.rank)}</em></p></span>
       <div className="buttons">
       {props.rank === "1" && <Button className="delete-club" onClick={props.deleteClub} variant="warning">Delete Club</Button>}
       <Button className="leave-club" onClick={props.eventHandler} variant="secondary">Leave Club</Button>
