7edccb1ec4c2de97d5570856f2ffc3941181c224
Jack Fox <jfox13@nd.edu>
2021-05-14 15:39:18 -0400

    added button groups for members

--- FrontEnd/src/Components/ClubAdmin/ClubMembership/Member.js
+++ FrontEnd/src/Components/ClubAdmin/ClubMembership/Member.js
@@ -1,8 +1,10 @@
 import React from "react";
 //import "./Announcement.css";
 //import { Link } from "react-router-dom";
+import "./Member.css";
 import { useState } from "react";
 import ProfilePage from "../../ProfilePage/ProfilePage";
+import {ButtonGroup, Button } from 'react-bootstrap';
 
 export default function Member(props) {
   const [show, setShow] = useState(false);
@@ -15,8 +17,11 @@ export default function Member(props) {
 
   return (
     <div className={show ? 'hidden' : ''}>
-      <p>{props.member_username}</p>
-      <button className="ann-delete-button" onClick={deleteAnnouncementHandler}>DELETE</button>
+      <p className="member-name">{props.member_username}</p>
+      <ButtonGroup aria-label="Basic example">
+        <Button size="sm" variant="outline-success">Promote</Button>
+        <Button size="sm" variant="outline-danger">Demote</Button>
+      </ButtonGroup>
     </div>
   );
 }
