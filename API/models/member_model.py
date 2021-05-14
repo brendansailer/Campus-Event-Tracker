b013cb98a036d90e214a32859aa4cdcd19fb11ca
Jack Fox <jfox13@nd.edu>
2021-05-14 16:31:39 -0400

    fixed member model to include rank

--- API/models/member_model.py
+++ API/models/member_model.py
@@ -1,5 +1,5 @@
 class Member:
-    def __init__(self, user_id, username):
+    def __init__(self, user_id, username, rank):
         self.user_id = user_id
         self.username = username
         self.rank = rank
