0c3136f30d09038ce4e46706fb711743dc116b87
brendansailer <57545584+brendansailer@users.noreply.github.com>
2021-05-15 17:46:34 -0400

    Fix glitch because topic names aren't guaranteed to be unique

--- API/schemas/club_schema.py
+++ API/schemas/club_schema.py
@@ -14,6 +14,7 @@ class ClubMemberSchema(ma.Schema):
 # Club Schema
 class ClubSchema(ma.Schema):
   topic = fields.String()
+  topic_id = fields.String()
   clubs = fields.Nested(ClubMemberSchema, many=True)
 
 # Init schema
